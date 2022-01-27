df012b4177339df822c740950b6ea59c272339dd
YooMinWoo <86340430+YooMinWoo@users.noreply.github.com>
2022-01-27 11:49:10 +0900

    Add files via upload

---
+++ 파이썬챌린지/#12 ~ #19(22.01.25)/[Yoo]파이썬 챌린지 챕터2(12-19).py
@@ -0,0 +1,98 @@
+#12번
+
+num1 = int(input("숫자를 입력하세요. : "))
+num2 = int(input("숫자를 입력하세요. : "))
+
+if num1 > num2 :
+    print(f"{num2}, {num1}")
+else :
+    print(f"{num1}, {num2}")
+
+#13번
+
+
+num = int(input("20보다 작은 숫자를 입력하세요. : "))
+
+if num >= 20 :
+    print("Too high")
+else :
+    print("Thank you")
+
+
+#14번
+
+
+num = int(input("10부터 20까지의 숫자를 입력하세요. : "))
+
+if 10 <= num <=20:
+    print("Thank you")
+else :
+    print("Incorrect answer")
+
+
+#15번
+
+
+color = input("좋아하는 색을 입력하세요. : ")
+if color == "red" or color == "RED" or color == "Red" :
+    print("I like red too")
+else :
+    print("I don't like that color, I prefer red")
+
+
+#16번
+
+
+rain = input("밖에 비가 오니? : ")
+
+rain = rain.lower()
+
+if rain == "yes" :
+    wind = input("바람이 많이 부니? : ")
+    wind = wind.lower()
+    if wind == "yes" :
+        print("It is too windy for an umbrella")
+    else :
+        print("Take an umbrella")
+else :
+    print("Enjoy your day")
+
+
+#17번
+
+age = int(input("몇 살이니? : "))
+
+if age >= 18 :
+    print("You can vote")
+elif age == 17 :
+    print("You can learn to drive")
+elif age == 16 :
+    print("You can buy a lottery ticket")
+elif age < 16 :
+    print("You can go Trick-or-Treating")
+
+
+#18번
+
+num = int(input("숫자를 입력하세요. : "))
+
+if num < 10 :
+    print("Too low")
+elif 10 <= num <= 20 :
+    print("Correct")
+else :
+    print("Too high")
+
+
+#19번
+
+num = int(input("1 또는 2 또는 3을 입력하세요. : "))
+
+if num == 1 :
+    print("Thank you")
+elif num == 2:
+    print("Well done")
+elif num == 3 :
+    print("Correct")
+else :
+    print("Error message")
