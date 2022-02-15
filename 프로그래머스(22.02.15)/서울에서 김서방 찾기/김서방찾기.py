a50e7af6f6eefd93b6332d5e4dbfe1d005864df5
JeonYeongJue <98301517+JeonYeongJue@users.noreply.github.com>
2022-02-15 14:45:33 +0900

    Add files via upload

---
+++ 프로그래머스(22.02.15)/서울에서 김서방 찾기/김서방찾기.py
@@ -0,0 +1,10 @@
+def solution(seoul):
+
+    for i in range(len(seoul)):
+        if seoul[1] == 'Kim':
+            answer = "김서방은 {}에 있다".format(i)
+
+    return answer
+
+
+# 코드 실행은 됐는데 제출 후 채점하기에서 점수를 안줌
